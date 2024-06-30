7dc7a54675c5664638279f960cb56bfb05ceb00c
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-06-30 23:26:12 +0200

    flask setup, jinja  templates; basic website setup

---
+++ website/auth.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, redirect, url_for
+
+
+auth = Blueprint("auth", __name__)
+
+
+
+@auth.route("/login")
+def login():
+    return render_template("login.html")
+
+@auth.route("/signup")
+def sign_up():
+    return render_template("signup.html")
+
+
+
+@auth.route("/signout")
+def logout():
+    return redirect(url_for("views.home"))
