55137b3f8d1aacd1ef48be99833e2e3f74d7b8df
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-07-10 11:42:33 +0200

    styling

--- website/models.py
+++ website/models.py
@@ -4,19 +4,38 @@ from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
+    """
+    User model to store user details.
+
+    Attributes:
+        id (int): Primary key for the user.
+        email (str): Unique email address of the user.
+        username (str): Unique username of the user.
+        password (str): Hashed password of the user.
+        date_created (datetime): The date and time when the user was created.
+        posts (list[Post]): Relationship to the Post model, representing the user's posts.
+    """
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    posts =  db.relationship('Post', backref='user', passive_deletes= True)
-    
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+
 
 class Post(db.Model):
-    id  = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Integer, nullable=False)
-    date_created =db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"),nullable=False)
-    
-    
-    
+    """
+    Post model to store post details.
+
+    Attributes:
+        id (int): Primary key for the post.
+        text (str): Content of the post.
+        date_created (datetime): The date and time when the post was created.
+        author (int): Foreign key to the User model, representing the post's author.
+    """
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
